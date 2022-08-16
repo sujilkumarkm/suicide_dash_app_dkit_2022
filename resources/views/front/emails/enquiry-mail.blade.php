1c23c5cbb6c6eee2b20a671cd5df9eb03e270004
Sujil Kumar K.M <kmsujilkumar@gmail.com>
2022-08-16 16:56:45 +0100

    email template fixes

--- resources/views/front/emails/enquiry-mail.blade.php
+++ resources/views/front/emails/enquiry-mail.blade.php
@@ -1,10 +1,10 @@
 @extends('front.emails.user-mail')
 @section('preText')
-    Hi, you have new message. {{$enquiry['name']}} , Message: {{$enquiry['message']}}
+    Hi Admin, you have new message. {{$enquiry['name']}} , Message: {{$enquiry['message']}}
 @endsection
 @section('body')
-    Hi ,<br/><br/>
-    You have a new enquiry<br/>
+    Hi Admin,<br/><br/>
+    You recieved a new feedback from our page visitor.<br/>
     Name: {{$enquiry['name']}}<br/>
     Email: {{$enquiry['email']}}<br/>
     Phone: {{$enquiry['phone']}}<br/>
